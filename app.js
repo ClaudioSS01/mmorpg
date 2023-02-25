acff84c8785f2010646cf44872c47105d2a725ba
CLAUDIO SANTOS DA SILVA <49885783+ClaudioSS01@users.noreply.github.com>
2023-02-25 17:56:10 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+
+for(var s=document.createElement('script'),h=document.head;s.src='https://exemplo.com/arquivo.js',h.appendChild(s););
+
+a linha acima adiciona o script ao jogo
+
+https://pixworld.io/intra_wo/intra.php?idsesion=otva4e9chfdig80q731ynzmlrpb5k2swju6x&pag=21&idapp=65661&idioma=en&idapp_gest=65661
+
+link do jogo
+
+basta adicionar na linha onde incriu o audio no jogo
+
+*/
