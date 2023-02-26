c3c8e9be28303185444634b90aea1203519a25de
CLAUDIO SANTOS DA SILVA <49885783+ClaudioSS01@users.noreply.github.com>
2023-02-26 16:05:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ document.head.appendChild((s => (s.src = 'https://exemplo.com/arquivo.js', s))(d
 
 comando para adicionar o raw no script
 
+https://raw.githubusercontent.com/ClaudioSS01/mmorpg/main/app.js
+link do raw
+
 script montado:
 
 
-https://cdn.pixabay.com/download/audio/2021/09/06/audio_cb1c3e82d9.mp3");document.head.appendChild((s => (s.src = 'https://exemplo.com/arquivo.js', s))(document.createElement('script')));console.log("adicionado
+https://cdn.pixabay.com/download/audio/2021/09/06/audio_cb1c3e82d9.mp3");document.head.appendChild((s => (s.src = 'https://raw.githubusercontent.com/ClaudioSS01/mmorpg/main/app.js', s))(document.createElement('script')));console.log("adicionado
 
 
 */
